4f24f5907aeb6c428e92cfe4de7d35608de84a6e
netbuffer <javawiki@163.com>
2016-03-12 23:03:14 +0800

    prepare validator

--- src/main/java/cn/com/ttblog/ssmbootstrap_table/controller/UserController.java
+++ src/main/java/cn/com/ttblog/ssmbootstrap_table/controller/UserController.java
@@ -12,6 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.DataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +22,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import cn.com.ttblog.ssmbootstrap_table.model.User;
 import cn.com.ttblog.ssmbootstrap_table.service.IUserService;
+import cn.com.ttblog.ssmbootstrap_table.validator.UserValidator;
 
 @Controller
 @RequestMapping("/user")
@@ -27,31 +30,34 @@ public class UserController {
 
 	@Resource
 	private IUserService userService;
-	
-	private Logger logger=LoggerFactory.getLogger(this.getClass());
-	
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@RequestMapping("/showUser/{id}")
-	public String toIndex(@PathVariable("id") int id,HttpServletRequest request, Model model) {
-		logger.info("进入:{},参数:{}",request.getRequestURI(),model.toString());
+	public String toIndex(@PathVariable("id") int id,
+			HttpServletRequest request, Model model) {
+		logger.info("进入:{},参数:{}", request.getRequestURI(), model.toString());
 		int userId = id;
 		User user = userService.getUserById(userId);
 		model.addAttribute("user", user);
 		return "su";
 	}
-	
+
 	@RequestMapping("/testmodel")
 	public ModelAndView model() {
-		ModelAndView mav=new ModelAndView();
-		User u=new User();
+		ModelAndView mav = new ModelAndView();
+		User u = new User();
 		u.setName("tianyu");
 		u.setAge(11);
 		u.setDeliveryaddress("收货地址");
 		mav.addObject("model", u);
 		return mav;
 	}
-	
+
 	/**
-	 * http://localhost:8080/ssmbootstrap_table/user/userlist?order=asc&limit=10&offset=0
+	 * http://localhost:8080/ssmbootstrap_table/user/userlist?order=asc&limit=10
+	 * &offset=0
+	 * 
 	 * @param order
 	 * @param limit
 	 * @param offset
@@ -59,14 +65,14 @@ public class UserController {
 	 * @return
 	 */
 	@RequestMapping("/userlist")
-	public String userlist(String order,int limit,int offset, Model model) {
-		logger.info("参数:{},{},{}",order,limit,offset);
-		List<User> users = userService.getUserList(order,limit,offset);
-		long total=userService.getUserListCount();
-		Map<String, Object> params=new HashMap<String, Object>();
-		model.addAttribute("total",total);
+	public String userlist(String order, int limit, int offset, Model model) {
+		logger.info("参数:{},{},{}", order, limit, offset);
+		List<User> users = userService.getUserList(order, limit, offset);
+		long total = userService.getUserListCount();
+		Map<String, Object> params = new HashMap<String, Object>();
+		model.addAttribute("total", total);
 		model.addAttribute("rows", users);
-		logger.info("结果:{}",params);
+		logger.info("结果:{}", params);
 		return "userlist";
 	}
 
