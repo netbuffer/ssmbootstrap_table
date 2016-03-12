6925a2b00815db5f272928a60127f2914350f276
netbuffer <javawiki@163.com>
2016-03-12 23:03:14 +0800

    prepare validator

--- src/main/java/cn/com/ttblog/ssmbootstrap_table/controller/RegisterController.java
+++ src/main/java/cn/com/ttblog/ssmbootstrap_table/controller/RegisterController.java
@@ -5,10 +5,14 @@ import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.DataBinder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cn.com.ttblog.ssmbootstrap_table.model.User;
 import cn.com.ttblog.ssmbootstrap_table.service.IUserService;
+import cn.com.ttblog.ssmbootstrap_table.validator.UserValidator;
 
 @Controller
 @RequestMapping("/register")
@@ -19,14 +23,19 @@ public class RegisterController {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@RequestMapping(value={"","/","/index"})
+	@InitBinder
+	public void initBinder(DataBinder binder) {
+		binder.setValidator(new UserValidator());
+	}
+
+	@RequestMapping(value = { "", "/", "/index" })
 	public String index() {
-		return "redirect:/register.html";
+		return "redirect:/registerinfo.html";
 	}
 
 	@RequestMapping("/save")
 	public String save(User user) {
-		user.setAdddate((int)(System.currentTimeMillis()/1000));
+		user.setAdddate((int) (System.currentTimeMillis() / 1000));
 		try {
 			userService.addUser(user);
 		} catch (Exception e) {
