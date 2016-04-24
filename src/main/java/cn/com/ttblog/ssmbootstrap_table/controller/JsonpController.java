592ebe1181381d2a88b0262dc85010a5887baaff
netbuffer <javawiki@163.com>
2016-04-24 19:57:11 +0800

    spring-jsonp

--- src/main/java/cn/com/ttblog/ssmbootstrap_table/controller/JsonpController.java
+++ src/main/java/cn/com/ttblog/ssmbootstrap_table/controller/JsonpController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
 import cn.com.ttblog.ssmbootstrap_table.service.IUserService;
 
 /**
@@ -82,6 +86,11 @@ public class JsonpController {
 			e.printStackTrace();
 			logger.debug("发生错误:{}",e);
 		}
+	}
+	
+	@RequestMapping("/tj/{id}")
+	public JSONObject t(@PathVariable Long id,HttpServletRequest request,HttpServletResponse response) {
+		return (JSONObject)JSON.toJSON(userService.getUserById(id));
 	}
 	
 	@RequestMapping("/str")
