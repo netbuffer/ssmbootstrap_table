2a732e9465dff337ad81291606cc32e18972f0a8
netbuffer <javawiki@163.com>
2016-04-26 10:53:04 +0800

    update test

--- src/test/java/cn/com/ttblog/ssmbootstrap_table/TestMyBatis.java
+++ src/test/java/cn/com/ttblog/ssmbootstrap_table/TestMyBatis.java
@@ -24,7 +24,7 @@ import com.alibaba.fastjson.JSON;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 // 表示继承了SpringJUnit4ClassRunner类
-@ContextConfiguration(locations = { "classpath:spring-mybatis.xml" })
+@ContextConfiguration(locations = { "classpath:spring-mybatis.xml","classpath:spring-context.xml" })
 public class TestMyBatis {
 
 	private static Logger logger = LoggerFactory.getLogger(TestMyBatis.class);
