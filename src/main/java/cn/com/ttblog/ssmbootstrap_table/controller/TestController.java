749e0b7535a566ccf7af03106dd8b61e94ca91cd
netbuffer <javawiki@163.com>
2016-05-21 22:41:57 +0800

    fastjson config MessageConverter

--- src/main/java/cn/com/ttblog/ssmbootstrap_table/controller/TestController.java
+++ src/main/java/cn/com/ttblog/ssmbootstrap_table/controller/TestController.java
@@ -1,7 +1,11 @@
 package cn.com.ttblog.ssmbootstrap_table.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+
 import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +14,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.alibaba.fastjson.JSONObject;
 
 @Controller
@@ -48,4 +53,16 @@ public class TestController {
 		return connectTime;
 	}
 	
+	/**
+	 * 直接写响应，会使用FastJsonHttpMessageConverter作json转换
+	 * 访问路径会直接以json输出
+	 * @return
+	 */
+	@RequestMapping(value = {"/getobj"})
+	public  @ResponseBody  Map<String, Object> getobj(){
+		Map<String, Object> res=new HashMap<String, Object>();
+		res.put("key1", "v1");
+		res.put("key2", "v2");
+		return res;
+	}
 }
