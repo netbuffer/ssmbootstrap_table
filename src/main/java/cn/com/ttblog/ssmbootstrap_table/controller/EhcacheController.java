70301828ecf55a439e28c07b06ea144328f59841
netbuffer <netbuffer>
2016-07-08 09:57:54 +0800

    update

--- src/main/java/cn/com/ttblog/ssmbootstrap_table/controller/EhcacheController.java
+++ src/main/java/cn/com/ttblog/ssmbootstrap_table/controller/EhcacheController.java
@@ -1,15 +1,15 @@
 package cn.com.ttblog.ssmbootstrap_table.controller;
 
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 
@@ -20,21 +20,20 @@ import net.sf.ehcache.CacheManager;
 @RequestMapping("/ehcache")
 public class EhcacheController {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	@Autowired  
-    private ApplicationContext applicationContext;
-
+//	@Autowired  
+//    private ApplicationContext applicationContext;
+	@Autowired
+	private CacheManager cacheManager;
 	@RequestMapping(value = {"/index/{cache}" })
 	public String index(@PathVariable("cache") String cache, ModelMap m) {
 		logger.debug("cache:{}", cache);
-		EhCacheCacheManager cacheManager=(EhCacheCacheManager) applicationContext.getBean("cacheManager");
 		if(cacheManager!=null){
-			m.put("caches", cacheManager.getCacheNames());
-			CacheManager cm=cacheManager.getCacheManager();
-			m.put("getDiskStorePath", cm.getDiskStorePath());
-			m.put("getName", cm.getName());
-//			m.put("getOriginalConfigurationText", cm.getOriginalConfigurationText());
-			m.put("getStatus", cm.getStatus());
-			Cache c=cm.getCache(cache);
+			m.put("caches",Arrays.deepToString(cacheManager.getCacheNames()));
+			m.put("getDiskStorePath", cacheManager.getDiskStorePath());
+			m.put("getName", cacheManager.getName());
+//			m.put("getOriginalConfigurationText", cacheManager.getOriginalConfigurationText());
+			m.put("getStatus", cacheManager.getStatus());
+			Cache c=cacheManager.getCache(cache);
 			if(c!=null){
 				m.put("getStatistics",c.getStatistics());
 				logger.debug("ehcache-{} getStatistics:{}",cache,c.getStatistics());
