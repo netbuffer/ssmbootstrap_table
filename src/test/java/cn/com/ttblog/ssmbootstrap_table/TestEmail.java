d7150931ce4725a79ad38a2292b351baee3155f1
netbuffer <netbuffer>
2016-07-28 16:14:45 +0800

    update

--- src/test/java/cn/com/ttblog/ssmbootstrap_table/TestEmail.java
+++ src/test/java/cn/com/ttblog/ssmbootstrap_table/TestEmail.java
@@ -14,20 +14,23 @@ import org.slf4j.LoggerFactory;
 public class TestEmail {
 	
 	private static Logger logger = LoggerFactory.getLogger(TestMyBatis.class);
-
+	private static final String user="user";
+	//使用授权码
+	private static final String pwd="pwd";
 	@Test
 //	http://commons.apache.org/proper/commons-email/userguide.html
+//	http://help.mail.163.com/faqDetail.do?code=d7a5dc8471cd0c0e8b4b8f4f8e49998b374173cfe9171305fa1ce630d7f67ac22dc0e9af8168582a
 	public void testSimpleTxt() throws EmailException {
 		Email email = new SimpleEmail();
 		email.setDebug(true);
-		email.setHostName("host");
+		email.setHostName("smtp.yeah.net");
 		email.setSmtpPort(25);
-		email.setAuthenticator(new DefaultAuthenticator("test", "test"));
-		email.setSSLOnConnect(true);
-		email.setFrom("test");
+		email.setAuthenticator(new DefaultAuthenticator(user,pwd ));
+		email.setSSLOnConnect(false);
+		email.setFrom(user);
 		email.setSubject("TestMail");
 		email.setMsg("This is a test mail ... :-)");
-		email.addTo("test");
+		email.addTo(user);
 		email.send();
 	}
 }
