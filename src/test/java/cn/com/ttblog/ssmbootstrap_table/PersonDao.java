e6519c75764210b1f534656b0d6576d348be4aa4
netbuffer <javawiki@163.com>
2016-08-19 16:01:11 +0800

    mockito/jwebunit

---
+++ src/test/java/cn/com/ttblog/ssmbootstrap_table/PersonDao.java
@@ -0,0 +1,11 @@
+package cn.com.ttblog.ssmbootstrap_table;
+
+/**
+ * @author Phat (Phillip) H. VU <vuhongphat@hotmail.com>
+ *
+ */
+public interface PersonDao {
+	public Person fetchPerson(Integer personID);
+
+	public void update(Person person);
+}
