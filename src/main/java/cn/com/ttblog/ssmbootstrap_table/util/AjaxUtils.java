73f5876bf895b21fc9de1d1fd7d3b7dc36ef1a97
netbuffer <javawiki@163.com>
2016-10-11 22:00:11 +0800

    update

--- src/main/java/cn/com/ttblog/ssmbootstrap_table/util/AjaxUtils.java
+++ src/main/java/cn/com/ttblog/ssmbootstrap_table/util/AjaxUtils.java
@@ -1,5 +1,8 @@
 package cn.com.ttblog.ssmbootstrap_table.util;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.context.request.WebRequest;
@@ -8,18 +11,42 @@ public class AjaxUtils {
 
 	public static boolean isAjaxRequest(WebRequest webRequest) {
 		String requestedWith = webRequest.getHeader("X-Requested-With");
-		return requestedWith != null ? "XMLHttpRequest".equals(requestedWith) : false;
+		return requestedWith != null ? "XMLHttpRequest".equals(requestedWith)
+				: false;
 	}
 
 	public static boolean isAjaxRequest(HttpServletRequest webRequest) {
 		String requestedWith = webRequest.getHeader("X-Requested-With");
-		return requestedWith != null ? "XMLHttpRequest".equals(requestedWith) : false;
+		return requestedWith != null ? "XMLHttpRequest".equals(requestedWith)
+				: false;
 	}
-	
+
 	public static boolean isAjaxUploadRequest(WebRequest webRequest) {
 		return webRequest.getParameter("ajaxUpload") != null;
 	}
-	
-	private AjaxUtils() {}
+
+	public static String getLocalIP() {
+		InetAddress addr = null;
+		try {
+			addr = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		byte[] ipAddr = addr.getAddress();
+		String ipAddrStr = "";
+		for (int i = 0; i < ipAddr.length; i++) {
+			if (i > 0) {
+				ipAddrStr += ".";
+			}
+			ipAddrStr += ipAddr[i] & 0xFF;
+		}
+		// System.out.println(ipAddrStr);
+		return ipAddrStr;
+	}
+
+	private AjaxUtils() {
+	}
 
 }
