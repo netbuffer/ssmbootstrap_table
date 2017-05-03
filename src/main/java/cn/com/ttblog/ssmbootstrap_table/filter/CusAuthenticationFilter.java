010a83d32da405f60797cc0c25ec9ab60032c002
netbuffer <javawiki@163.com>
2017-05-03 14:30:10 +0800

    custom redirect

--- src/main/java/cn/com/ttblog/ssmbootstrap_table/filter/CusAuthenticationFilter.java
+++ src/main/java/cn/com/ttblog/ssmbootstrap_table/filter/CusAuthenticationFilter.java
@@ -2,15 +2,19 @@ package cn.com.ttblog.ssmbootstrap_table.filter;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+import org.apache.shiro.web.util.WebUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component(value="cfauth")
 public class CusAuthenticationFilter extends FormAuthenticationFilter {
@@ -32,9 +36,29 @@ public class CusAuthenticationFilter extends FormAuthenticationFilter {
 	@Override
 	protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
 		LOG.debug("login accessdenied");
-		return super.onAccessDenied(request, response);
+		if(this.isLoginRequest(request, response)) {
+			if(this.isLoginSubmission(request, response)) {
+				LOG.debug("LOGin submission detected.  Attempting to execute LOGin.");
+				return super.executeLogin(request, response);
+			} else {
+				LOG.debug("LOGin page view.");
+				return true;
+			}
+		} else {
+			LOG.debug("Attempting to access a path which requires authentication.  Forwarding to the Authentication url [" + this.getLoginUrl() + "]");
+			this.saveRequestAndRedirectToLogin(request, response);
+			return false;
+		}
 	}
-	
+
+	@Override
+	protected void redirectToLogin(ServletRequest request, ServletResponse response) throws IOException {
+		String loginUrl = this.getLoginUrl();
+		//可以在这里面做未登录地址的跳转规则，跳转到不同的页面
+		LOG.debug("未登录要跳转到的地址:{},req uri:{}",loginUrl,((HttpServletRequest)request).getRequestURI());
+		WebUtils.issueRedirect(request, response, loginUrl);
+	}
+
 	@Override
 	protected boolean onLoginSuccess(AuthenticationToken token, Subject subject,
             ServletRequest request, ServletResponse response) throws Exception {
