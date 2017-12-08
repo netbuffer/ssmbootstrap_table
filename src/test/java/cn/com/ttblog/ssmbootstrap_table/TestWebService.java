3becf5a7046aef8d54cae6489e045e436e33bb58
netbuffer <javawiki@163.com>
2017-12-08 15:08:37 +0800

    update

--- src/test/java/cn/com/ttblog/ssmbootstrap_table/TestWebService.java
+++ src/test/java/cn/com/ttblog/ssmbootstrap_table/TestWebService.java
@@ -1,5 +1,7 @@
 package cn.com.ttblog.ssmbootstrap_table;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 import org.junit.Test;
@@ -14,11 +16,12 @@ public class TestWebService {
     private static Logger LOGGER = LoggerFactory.getLogger(TestWebService.class);
 
     @Test
-    public void test() throws Exception {
+    public void testInvoke() throws Exception {
         JaxWsDynamicClientFactory factory = JaxWsDynamicClientFactory.newInstance();
         Client client = factory.createClient("http://localhost:8080/ssm/cxf/userservice?wsdl");
         QName opName = new QName("http://ttblog.com/ssmsoap", "findById");
-        Object[] results = client.invoke("findById", 1L);
-        LOGGER.info("results:{}", Arrays.deepToString(results));
+        Object[] results = client.invoke("findById", 2L);
+        LOGGER.info("results:{},result[0]:{}", Arrays.deepToString(results), ToStringBuilder.reflectionToString(results[0], ToStringStyle.JSON_STYLE));
+
     }
 }
